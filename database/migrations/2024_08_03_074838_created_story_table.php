273119dd00945bf3b35a190903e8db22bf913bfd
TreeMarch <m5c0traan@gmail.com>
2024-08-11 23:14:20 +0700

    migrate them truong img_url va avatar_url

--- database/migrations/2024_08_03_074838_created_story_table.php
+++ database/migrations/2024_08_03_074838_created_story_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('creted_story');
+        Schema::dropIfExists('created_story');
     }
 };
